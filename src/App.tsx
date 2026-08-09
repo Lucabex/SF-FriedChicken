d0bbc8de25e0c848b4051b449fa19ef18d209053
Luca Bercioux <Lucabex@gmail.com>
2026-08-09 19:56:52 +0100

    complete Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import type { menuItem } from "./types/menuItem";
+
 function App() {
+  const menuItems: menuItem[] = [
+    { name: "1 pc Chicken & Fries", price: 3.5 },
+    { name: "2 pcs Chicken & Fries", price: 4.95 },
+    { name: "3 pcs Chicken & Fries", price: 6.85 },
+    { name: "4 pcs Chicken & 2 Fries", price: 9.5 },
+    { name: "6 pcs Chicken & 3 Fries", price: 14.0 },
+    { name: "8 pcs Chicken & 4 Fries", price: 17.95 },
+    { name: "12 pcs Chicken & 4 Fries", price: 24.5 },
+    { name: "Chicken Feast", price: 12.5 },
+    { name: "Family Pack", price: 16.5 },
+    { name: "Bargain Bucket", price: 20.0 },
+    { name: "Party Pack", price: 26.5 },
+    { name: "Mega Pack", price: 31.95 },
+    { name: "Donor Kebab (Regular)", price: 6.0 },
+    { name: "Donor Kebab (Large)", price: 7.5 },
+    { name: 'Pizza 9" (Buy 1)', price: 3.95 },
+    { name: "Garlic Bread", price: 1.95 },
+  ];
   return (
     <>
       <header>
         <div className="topHead">
           <span>√ HALAL</span>
+          <span>حلال</span>
 
           <a href="01582 519519">Order by phone: 01582 519519</a>
         </div>
@@ -24,7 +45,7 @@ function App() {
       </header>
       <section className="hero">
         <div className="heroText">
-          <span className="oblText">Very Flicking Good</span>
+          <span className="oblText">Lip Licking Flavour</span>
           <h2>
             Fried <strong>Fresh</strong>
             <br />
@@ -59,6 +80,14 @@ function App() {
           <div className="menuTitle">
             <span>Straight off the board</span>
             <h3>Chicken & Fries</h3>
+          </div>
+          <div className="menuGrid">
+            {menuItems.map((item) => (
+              <div className="itemBox" key={item.name}>
+                <span>{item.name}</span>
+                <span id="priceBox">£ {item.price}</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/types/menuItem.ts
@@ -0,0 +1,14 @@
+export type menuItem = {
+  name: string;
+  price: number;
+};
+
+export type bigOffer = {
+  offerName: string;
+  item1: string;
+  item2: string;
+  item3: string;
+  item4?: string;
+  item5?: string;
+  price: number;
+};
